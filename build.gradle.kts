56c9bd4bf1bbfbf57167bc0b2b4287d7f8faf989
troyhen <troyhen@gmail.com>
2019-01-24 12:21:36 -0700

    Update kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 buildscript {
-    val kotlinVersion = "1.3.11"
+    val kotlinVersion = "1.3.20"
     repositories {
         google()
         jcenter()
         
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.4.0-alpha10")
+        classpath("com.android.tools.build:gradle:3.4.0-beta01")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         // NOTE: Do not place your application dependencies here; they belong in the individual module build.gradle files
     }
